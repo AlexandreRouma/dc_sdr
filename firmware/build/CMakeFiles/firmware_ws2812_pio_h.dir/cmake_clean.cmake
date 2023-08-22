96740d834e4591d31a7b843f296c333dbe8bd8b9
AlexandreRouma <alexandre.rouma@gmail.com>
2023-08-22 02:52:34 +0200

    Delete firmware/build directory

--- firmware/build/CMakeFiles/firmware_ws2812_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/ryzerth/dc_sdr/firmware/lib/ws2812/ws2812.pio.h"
-  "CMakeFiles/firmware_ws2812_pio_h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/firmware_ws2812_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
